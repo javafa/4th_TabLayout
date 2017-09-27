1d1f57fcddbd7d9722737dc5403466fdbede3e60
javafa <nigne2016@gmail.com>
2017-09-27 16:46:50 +0900

    Initial commit

---
+++ app/src/main/java/com/veryworks/android/tablayout/MainActivity.java
@@ -0,0 +1,54 @@
+package com.veryworks.android.tablayout;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTabLayout();
+        setViewPager();
+
+        setListener();
+    }
+
+    private void setListener(){
+        // 탭레이아웃을 ViewPager와 연결
+        tabLayout.addOnTabSelectedListener(
+                new TabLayout.ViewPagerOnTabSelectedListener(viewPager)
+        );
+    }
+
+    private void setTabLayout(){
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab( tabLayout.newTab().setText("One") );
+        tabLayout.addTab( tabLayout.newTab().setText("Two") );
+        tabLayout.addTab( tabLayout.newTab().setText("Three") );
+        tabLayout.addTab( tabLayout.newTab().setText("Four") );
+
+    }
+
+    private void setViewPager(){
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        List<Fragment> data = new ArrayList<>();
+        data.add(new OneFragment());
+        data.add(new TwoFragment());
+        data.add(new ThreeFragment());
+        data.add(new FourFragment());
+        CustomAdapter adapter = new CustomAdapter(getSupportFragmentManager(), data);
+
+        viewPager.setAdapter(adapter);
+    }
+}
